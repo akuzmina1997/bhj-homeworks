8dc76349d3694c5132c4d543434c628686c505a3
Anastasiya Kuzmina <55270579+akuzmina1997@users.noreply.github.com>
2020-10-27 00:38:15 +0300

    ver.3

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,30 +2,32 @@
 
 const menuLink = document.getElementsByClassName("menu__link");
 const menuLength = menuLink.length;
+const menuParentLi = menuLink[i].parentElement;
+const menuLi = menuParentLi.getElementsByTagName("ul")[0];
 
-function closeMenus() {
+function closeMenu() {
     const menuElements = document.querySelectorAll("li ul");
     for (let i = 0; i < menuElements.length; i++) {
         menuElements[i].className = "menu menu_sub";
     }
 }
 
-function changeSubmenuStatus(menuLi) {
-    if (menuLi.classList.contains("menu_active")) 
-        menuLi.classList.remove("menu_active");
+function changeMenu(menuLi) {
+    if (menuLi.classList.contains("menu_active")) {
+        //menuLi.classList.remove("menu_active");
+        menuLi.classList.toggle("menu_active");
 
-    else {
-        menuLi.classList.add("menu_active");
+    //else {
+        //menuLi.classList.add("menu_active");
     }
 }
 
 for (let i = 0; i < menuLength; i++) {
     menuLink[i].onclick = () => {
-        const menuParentLi = menuLink[i].parentElement;
-        menuLi = menuParentLi.getElementsByTagName("ul")[0];
+        //menuLi = menuParentLi.getElementsByTagName("ul")[0];
 
         if (menuLi) {
-            changeSubmenuStatus(menuLi);
+            changeMenu(menuLi);
             return false;
         }
     }
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,33 +1,60 @@
 "use strict";
 
-const sliderArrow = document.getElementsByClassName("slider__arrow");
-const sliderItems = Array.from(document.querySelectorAll(".slider__items div"));
+const slider = Array.from(document.querySelectorAll('.slider__item'));
+const arrowRight = document.querySelector('.slider__arrow_next');
+const arrowLeft = document.querySelector('.slider__arrow_prev');
 
-function activeSlide() {
-    const currentSlide = sliderItems.findIndex( (elem) => 
-        elem.classList.contains("slider__item_active")
-    );
+let index = slider.findIndex(element => {
+    console.log(element);
+    return element.classList.contains('slider__item_active');
+});
 
-    sliderItems[currentSlide].classList.remove("slider__item_active");
-    return currentSlide;
-}
+arrowRight.addEventListener('click', ()=>{
+    slider[index].classList.remove('slider__item_active');
+    (index === slider.length - 1)?
+        (index = 0,
+        slider[index].classList.add('slider__item_active')):
+        (slider[index + 1].classList.add('slider__item_active'));
+    index++;
+})
+
+arrowLeft.addEventListener('click', ()=>{
+    slider[index].classList.remove('slider__item_active');
+    index--;
+    (index === 0)?
+        (index = slider.length - 1,
+        slider[index].classList.add('slider__item_active')):
+        (slider[index].classList.add('slider__item_active'));
+})
+
+//const sliderArrow = document.getElementsByClassName("slider__arrow");
+//const sliderItems = Array.from(document.querySelectorAll(".slider__items div"));
+
+//function activeSlide() {
+    //const currentSlide = sliderItems.findIndex( (elem) => 
+        //elem.classList.contains("slider__item_active")
+    //);
+
+    //sliderItems[currentSlide].classList.remove("slider__item_active");
+    //return currentSlide;
+//}
 
 // кнопка направо
-sliderArrow[1].onclick = () => {
+//sliderArrow[1].onclick = () => {
 
-    const currentSlide = activeSlide();
-    if (currentSlide === sliderItems.length - 1)
-        currentSlide = -1;
+    //const currentSlide = activeSlide();
+    //if (currentSlide === sliderItems.length - 1)
+        //currentSlide = -1;
 
-    sliderItems[currentSlide + 1].classList.add("slider__item_active");
-}
+    //sliderItems[currentSlide + 1].classList.add("slider__item_active");
+//}
 
 //кнопка налево
-sliderArrow[0].onclick = () => {
+//sliderArrow[0].onclick = () => {
 
-    const currentSlide = activeSlide();
-    if (currentSlide === 0)
-        currentSlide = sliderItems.length;
+    //const currentSlide = activeSlide();
+    //if (currentSlide === 0)
+        //currentSlide = sliderItems.length;
 
-    sliderItems[currentSlide - 1].classList.add("slider__item_active");
-}
+    //sliderItems[currentSlide - 1].classList.add("slider__item_active");
+//}
