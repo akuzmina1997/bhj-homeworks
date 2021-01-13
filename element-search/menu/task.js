7af4dd596f8007f9c64d2c04cbdff4e149f264b9
Anastasiya Kuzmina <55270579+akuzmina1997@users.noreply.github.com>
2021-01-13 04:18:23 +0300

    menu task renewed

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,16 +1,22 @@
 "use strict";
 
 //ссылки в меню
-const menuLink = Array.from(document.querySelectorAll('.menu__link'));
+const menuLink = Array.from(document.querySelectorAll(".menu__link"));
 
-menuLink.forEach(elem => {
+menuLink.forEach(elem =>{
+    elem.onclick = function () {
 
-    //const menuActive = document.querySelector('.menu_active');
-    elem.addEventListener('click', (event) => {
-        event.preventDefault();
+        const menuActive = document.querySelector(".menu_active");
 
-        if(elem.nextElementSibling){
-            elem.nextElementSibling.classList.toggle('menu_active');
-        } 
-    });
-});
+        if(menuActive) {
+            if(menuActive !== elem.nextElementSibling) {
+                menuActive.classList.remove("menu_active");
+
+            }
+        }
+
+            elem.nextElementSibling.classList.toggle("menu_active");
+            return false;
+        }
+    
+})
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -13,53 +13,10 @@ arrowRight.addEventListener('click', ()=>{
     slider[index].classList.remove('slider__item_active');
     index = index === slider.length - 1 ? 0 : index + 1; 
     slider[index].classList.add('slider__item_active'); 
-
-    //(index === slider.length - 1)?
-        //(index = 0,
-        //slider[index].classList.add('slider__item_active')):
-        //(slider[index + 1].classList.add('slider__item_active'));
-    //index++;
 });
 
 arrowLeft.addEventListener('click', ()=>{
     slider[index].classList.remove('slider__item_active');
     index = index === 0 ? slider.length - 1 : index - 1; 
     slider[index].classList.add('slider__item_active'); 
-    //index--;
-    //(index === 0)?
-        //(index = slider.length - 1,
-        //slider[index].classList.add('slider__item_active')):
-        //(slider[index].classList.add('slider__item_active'));
 });
-
-//const sliderArrow = document.getElementsByClassName("slider__arrow");
-//const sliderItems = Array.from(document.querySelectorAll(".slider__items div"));
-
-//function activeSlide() {
-    //const currentSlide = sliderItems.findIndex( (elem) => 
-        //elem.classList.contains("slider__item_active")
-    //);
-
-    //sliderItems[currentSlide].classList.remove("slider__item_active");
-    //return currentSlide;
-//}
-
-// кнопка направо
-//sliderArrow[1].onclick = () => {
-
-    //const currentSlide = activeSlide();
-    //if (currentSlide === sliderItems.length - 1)
-        //currentSlide = -1;
-
-    //sliderItems[currentSlide + 1].classList.add("slider__item_active");
-//}
-
-//кнопка налево
-//sliderArrow[0].onclick = () => {
-
-    //const currentSlide = activeSlide();
-    //if (currentSlide === 0)
-        //currentSlide = sliderItems.length;
-
-    //sliderItems[currentSlide - 1].classList.add("slider__item_active");
-//}
